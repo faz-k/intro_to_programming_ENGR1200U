fe59ce26d244bf23d9defe603f2e43285da6d8ca
XSykotroz <sykotroz@protonmail.com>
2026-01-25 17:21:12 -0500

    Add tasks for building, running, and cleaning C++ project; include initial main.cpp

---
+++ tutorial_1/main.cpp
@@ -0,0 +1,11 @@
+// Course: ENGR 1200U
+// Tutorial: 01 - Task 1
+
+#include <iostream> // for std::cout and std::endl
+
+int main()
+{
+    std::cout << "ENGR 1200 - Tutorial 01" << std::endl;
+    std::cout << "Status: First run successful" << std::endl;
+    return 0;
+}
